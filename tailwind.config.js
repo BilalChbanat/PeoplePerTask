9a0ce2a03f899a0da6a01bd7a792519cd9b338a4
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-11-01 14:14:56 +0100

    whyChooseUs section is done+darkMode in about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,13 @@ module.exports = {
         cardGrey: '#161E2D',
         textGrey: '#8E95A1',
         bluepeople: '#6366F1',
-        seperator: '#3F444E'
+        seperator: '#3F444E',
+        cardDarkMod: '#161E2D',
+        pColorWightMode: '#363848',
+        pColorDarkMode: '#A9A9A9',
+      },
+      boxShadow: {
+        'shadowBox': '5px 5px 25px rgba(1 1 1 / 20%);',
       },
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
