67c01c917e9db5a56bbf878487caf884f2791d7f
Yassir Elkhaili <elkhailiyassir@gmail.com>
2023-11-23 22:11:31 +0100

    added delete and read functionality

--- src/scripts/sandbox.ts
+++ src/scripts/sandbox.ts
@@ -61,6 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
     }
+
   //handle contactform submission
   contactForm && handleContactFormSubmission();
   //dark/light mode switcher
